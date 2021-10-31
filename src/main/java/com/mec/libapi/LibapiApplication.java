e8210baa467cad86d9280003eec01ac9228607f8
Ilyass Elghazi <61422528+elyassgh@users.noreply.github.com>
2021-10-31 13:26:27 +0100

    Project structure (#1)
    * test push in blocked workflow
    
    * project structure

--- src/main/java/com/mec/libapi/LibapiApplication.java
+++ src/main/java/com/mec/libapi/LibapiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibapiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LibapiApplication.class, args);
 	}
-
 }
