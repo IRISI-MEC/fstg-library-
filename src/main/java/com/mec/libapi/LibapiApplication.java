ae2fd4e952a840f3ec0a52c5939b249ab2e0f002
elyassgh <elghazi.elyass@gmail.com>
2021-10-31 12:18:03 +0100

    test push in blocked workflow

--- src/main/java/com/mec/libapi/LibapiApplication.java
+++ src/main/java/com/mec/libapi/LibapiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibapiApplication {
-
+	// test push in blocked work flow !
 	public static void main(String[] args) {
 		SpringApplication.run(LibapiApplication.class, args);
 	}
