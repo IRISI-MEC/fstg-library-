2923148041d482ec780478b8fa613b7509f0a9cc
elyassgh <elghazi.elyass@gmail.com>
2021-10-31 17:41:31 +0100

    fixing pom file

--- src/main/java/com/mec/libapi/repository/NCaseRepository.java
+++ src/main/java/com/mec/libapi/repository/NCaseRepository.java
@@ -2,9 +2,11 @@ package com.mec.libapi.repository;
 
 import com.mec.libapi.entity.NCase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface NCaseRepository extends JpaRepository<NCase, Long> {
 
     Optional<NCase> findByCode(String code);
